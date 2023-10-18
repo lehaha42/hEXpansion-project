52c9397c57729eb0ca7922ef13b46bbafc283cf1
lehaha42 <shihovalexey2020@gmail.com>
2023-10-18 22:54:49 +1000

    move scale reduction in cell

--- world.py
+++ world.py
@@ -5,8 +5,9 @@ from cell import Cell
 class World:
     def __init__(self, app, size=8):
         self.app = app
-        self.size = size
-        self.arr = [[Cell(self.app, size * 1.5 - 1 > i + j >= size / 2 - 1)
+        self.size = size * 2 + 1
+        # create basic world
+        self.arr = [[Cell(self.app, [i, j], self.size * 1.5 - 1 > i + j >= self.size / 2 - 1)
                      for i in range(self.size)]
                     for j in range(self.size)]
 
@@ -14,4 +15,4 @@ class World:
     def show(self, offset, scale):
         for i in range(self.size):
             for j in range(self.size):
-                self.arr[j][i].show([(i + j/2)*scale + offset[0], j*scale + offset[1]], [scale*SCALE_REDUCTION, scale*SCALE_REDUCTION])
+                self.arr[j][i].show([(i + j/2)*scale + offset[0], j*scale + offset[1]], scale)
