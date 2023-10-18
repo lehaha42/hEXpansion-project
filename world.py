d001b6cbca5d9df16c7c670e39b1e3d32508e3b2
lehaha42 <shihovalexey2020@gmail.com>
2023-10-18 21:34:47 +1000

    Revert "move scale and offset to world.py"
    This reverts commit 5b897debbb0cd5ab040825f66f1469d59f7b1e7d.

--- world.py
+++ world.py
@@ -3,20 +3,14 @@ from cell import Cell
 
 
 class World:
-    def __init__(self, app, size=8, scale=100, offset=[0, 0]):
+    def __init__(self, app, size=8):
         self.app = app
         self.size = size
-        self.scale = scale
-        self.offset = offset
         self.arr = [[Cell(self.app, size * 1.5 - 1 > i + j >= size / 2 - 1)
                      for i in range(self.size)]
                     for j in range(self.size)]
 
-    def set(self, offset=self.offset, scale=self.scale):
-        self.offset = offset
-        self.scale = scale
-
-    def show(self):
+    def show(self, offset, scale):
         for i in range(self.size):
             for j in range(self.size):
-                self.arr[j][i].show([(i + j/2)*self.scale + self.offset[0], j*self.scale + self.offset[1]], [self.scale*SCALE_REDUCTION, self.scale*SCALE_REDUCTION])
+                self.arr[j][i].show([(i + j/2)*scale + offset[0], j*scale + offset[1]], [scale*SCALE_REDUCTION, scale*SCALE_REDUCTION])
