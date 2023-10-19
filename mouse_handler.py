b0dc93ec32aa8acdc2cf2e0f8f1d612399537b96
lehaha42 <shihovalexey2020@gmail.com>
2023-10-19 22:17:57 +1000

    testing mouse_handler.py

--- mouse_handler.py
+++ mouse_handler.py
@@ -2,5 +2,19 @@ from settings import *
 
 
 class MouseHandler:
-    def __init__(self):
-        pass
+    def __init__(self, app):
+        self.app = app
+
+    def update(self, events):
+        for event in events:
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    print('1')
+                if event.button == 2:
+                    print('2')
+                if event.button == 3:
+                    print('3')
+                if event.button == 4:
+                    print('4')
+                if event.button == 5:
+                    print('5')
