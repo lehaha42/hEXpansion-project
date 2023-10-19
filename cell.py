8937f94ad5f4c95037de082dd770a826f269d3c7
lehaha42 <shihovalexey2020@gmail.com>
2023-10-19 17:48:22 +1000

    new functions: update_connections, get_position update show functions

--- cell.py
+++ cell.py
@@ -1,22 +1,36 @@
 from settings import *
 
 
+SCALE = 0.5
+
+
 class Cell:
-    def __init__(self, app, position=[0, 0], exist=True):
+    def __init__(self, app, exist=True):
         self.app = app
-        self.position = position
         self.exist = exist
         self.connect = [False, False, False, False, False, False]
 
-    def update_connections(self):
-        self.connect = [True, True, True, True, True, True]
+    def update_connections(self, connects):
+        self.connect = connects
 
-    def show(self, pos, size):
+    @staticmethod
+    def get_position(offset, pos, scale):
+        i, j = pos
+        return [(i + j/2)*scale + offset[0], j*scale + offset[1]]
+
+    def show_cell(self, offset, pos, size):
         if self.exist:
-            pg.draw.rect(self.app.screen, WHITE, [*pos, size * SCALE_REDUCTION, size * SCALE_REDUCTION])
-            # pos[0]-size[0]/2, pos[1]-size[1]/2
-            neighbours = [[-1, 0], [-1, -1], [0, -1], [1, 0], [1, 0], [0, 1]]
+            cell_pos = self.get_position(offset, pos, size)
+            pg.draw.rect(self.app.screen, WHITE, [cell_pos[0] - size * SCALE / 2 + 2,
+                                                  cell_pos[1] - size * SCALE / 2 + 2,
+                                                  size * SCALE, size * SCALE])
+
+    def show_connections(self, offset, pos, size):
+        if self.exist:
+            cell_pos = self.get_position(offset, pos, size)
+            neighbours = [[-1, 0], [-1, 1], [0, 1], [1, 0], [1, -1], [0, -1]]
             for i in range(6):
                 if self.connect[i]:
-                    x, y = self.position[0]+neighbours[i][0], self.position[1]+neighbours[i][1]
-                    pg.draw.line(self.app.screen, WHITE, self.position, self.app.world.arr[x][y].position)
+                    neigh_pos = self.get_position(offset, [pos[0] + neighbours[i][0], pos[1] + neighbours[i][1]], size)
+                    if self.app.world.is_exist([pos[0] + neighbours[i][0], pos[1] + neighbours[i][1]]):
+                        pg.draw.line(self.app.screen, GRAY, cell_pos, neigh_pos, 4)
