c5082f88724104a137bd1aaa91d50daeda33b222
lehaha42 <shihovalexey2020@gmail.com>
2023-10-19 22:18:40 +1000

    move scale and offset in world.py add update

--- world.py
+++ world.py
@@ -3,7 +3,9 @@ from cell import Cell
 
 
 class World:
-    def __init__(self, app, size=7):
+    def __init__(self, app, size=7, scale=50, offset=[50, 50]):
+        self.scale = scale
+        self.offset = offset
         self.app = app
         self.size = size * 2 + 1
         # create basic world
@@ -21,7 +23,10 @@ class World:
     def is_exist(self, pos):
         return self.basic_rule(pos) and 0 <= pos[0] < self.size and 0 <= pos[1] < self.size
 
-    def show(self, offset, scale):
+    def show(self):
         for i in range(self.size):
             for j in range(self.size):
                 self.arr[j][i].show(offset, [i, j], scale)
+
+    def update(self):
+        pass
