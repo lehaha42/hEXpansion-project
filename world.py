cb6e1c84f7ad17933928e32454ce7f32f19e0456
lehaha42 <shihovalexey2020@gmail.com>
2023-10-19 17:50:45 +1000

    new functions: basic_rule, is_exist layered show

--- world.py
+++ world.py
@@ -3,16 +3,29 @@ from cell import Cell
 
 
 class World:
-    def __init__(self, app, size=8):
+    def __init__(self, app, size=7):
         self.app = app
         self.size = size * 2 + 1
         # create basic world
-        self.arr = [[Cell(self.app, [i, j], self.size * 1.5 - 1 > i + j >= self.size / 2 - 1)
+        self.arr = [[Cell(self.app, self.basic_rule([i, j]))
                      for i in range(self.size)]
                     for j in range(self.size)]
+        for i in range(self.size):
+            for j in range(self.size):
+                self.arr[i][j].update_connections([1, 1, 1, 1, 1, 1])
 
-#ПУСТЬ ТАК ОСТАНЕТСЯ
+    def basic_rule(self, pos):
+        i, j = pos
+        return self.size * 1.5 - 1 > i + j >= self.size / 2 - 1
+
+    def is_exist(self, pos):
+        return self.basic_rule(pos) and 0 <= pos[0] < self.size and 0 <= pos[1] < self.size
+
     def show(self, offset, scale):
         for i in range(self.size):
             for j in range(self.size):
-                self.arr[j][i].show([(i + j/2)*scale + offset[0], j*scale + offset[1]], scale)
+                self.arr[j][i].show_connections(offset, [i, j], scale)
+
+        for i in range(self.size):
+            for j in range(self.size):
+                self.arr[j][i].show_cell(offset, [i, j], scale)
