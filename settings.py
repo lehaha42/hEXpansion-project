9a6d5d37c9e66dab6a5efcb9a42b3f9f74405af7
lehaha42 <shihovalexey2020@gmail.com>
2023-10-19 17:46:44 +1000

    more colors

--- settings.py
+++ settings.py
@@ -7,7 +7,9 @@ NAME = 'hEXpansion'
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+GRAY = (150, 150, 150)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
 
 BACKGROUND = BLACK
-
-SCALE_REDUCTION = 0.3
