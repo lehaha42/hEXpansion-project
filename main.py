5bf1507b7965c26828150bb254d56a846e7be37b
lehaha42 <shihovalexey2020@gmail.com>
2023-10-19 22:17:38 +1000

    change events add event_handler

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from settings import *
 from world import World
-from mouse_handler import *
+from mouse_handler import MouseHandler
 
 
 class App:
     def __init__(self):
         pg.init()
         self.screen = pg.display.set_mode(WIN_RES)
-        #pg.display.set_caption(NAME)
+        pg.display.set_caption(NAME)
 
         self.clock = pg.time.Clock()
         self.delta_time = 0
@@ -18,8 +18,7 @@ class App:
 
     def on_init(self):
         self.world = World(self)
-        self.scale = 50
-        self.offset = [50, 50]
+        self.mouse_handler = MouseHandler(self)
 
     def update(self):
         self.delta_time = self.clock.tick()
@@ -27,13 +26,17 @@ class App:
         pg.display.set_caption(f'FPS: {self.clock.get_fps():.0f}')
 
     def handle_events(self):
-        for event in pg.event.get():
+        events = pg.event.get()
+        for event in events:
             if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
                 self.is_running = False
 
+        self.mouse_handler.update(events)
+
     def render(self):
         self.screen.fill(BACKGROUND)
-        self.world.show(self.offset, self.scale)
+        #self.world.show() (testing)
+        pg.draw.circle(self.screen, WHITE, self.world.offset, self.world.scale)
         pg.display.flip()
 
     def run(self):
