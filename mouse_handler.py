91bdeb4054236d2e671d4a19481a4aecfd1ca2db
lehaha42 <shihovalexey2020@gmail.com>
2023-10-20 16:03:53 +1000

    update

--- mouse_handler.py
+++ mouse_handler.py
@@ -4,17 +4,27 @@ from settings import *
 class MouseHandler:
     def __init__(self, app):
         self.app = app
+        self.pressed = False
+        self.pos = [0, 0]
+        self.prev_pos = [0, 0]
 
     def update(self, events):
+        self.prev_pos = (self.pos[0], self.pos[1])
+        self.pos = pg.mouse.get_pos()
         for event in events:
             if event.type == pg.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    print('1')
-                if event.button == 2:
-                    print('2')
                 if event.button == 3:
-                    print('3')
+                    self.pressed = True
                 if event.button == 4:
-                    print('4')
+                    self.app.world.zoom('in', self.pos)
                 if event.button == 5:
-                    print('5')
+                    self.app.world.zoom('out', self.pos)
+            if event.type == pg.MOUSEBUTTONUP:
+                if event.button == 3:
+                    self.pressed = False
+
+        if self.pressed:
+            rel_pos = [self.pos[0] - self.prev_pos[0], self.pos[1] - self.prev_pos[1]]
+            self.app.world.move_for(rel_pos)
+
+
