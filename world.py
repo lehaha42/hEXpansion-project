6d03588fa5fa670a39d9ebe2c8d60543a69c2bf4
lehaha42 <shihovalexey2020@gmail.com>
2023-10-20 16:02:46 +1000

    add move function work on zoom

--- world.py
+++ world.py
@@ -23,10 +23,19 @@ class World:
     def is_exist(self, pos):
         return self.basic_rule(pos) and 0 <= pos[0] < self.size and 0 <= pos[1] < self.size
 
+    def move_for(self, pos):
+        self.offset = [self.offset[0] + pos[0], self.offset[1] + pos[1]]
+
+    def zoom(self, direct, pos):
+        if direct == 'in':
+            pass
+        else:
+            pass
+
     def show(self):
         for i in range(self.size):
             for j in range(self.size):
-                self.arr[j][i].show(offset, [i, j], scale)
+                self.arr[j][i].show(self.offset, [i, j], self.scale)
 
     def update(self):
         pass
