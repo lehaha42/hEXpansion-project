8b1867295837a697875ee1c6e823a2dad3b285be
lehaha42 <shihovalexey2020@gmail.com>
2023-10-22 12:40:27 +1000

    test

--- menus_builder.py
+++ menus_builder.py
@@ -6,9 +6,11 @@ from button import Button
 def build_menus(app):
 
     def f(app, val):
-        app.is_running = val
+        app.render_world = val
 
-    button = Button(app, f, [app, False], pos=[10, 10], size=[50, 50])
-    menu1 = Menu(app, [button], pos=[10, 10], size=[100, 100])
+    button1 = Button(app, f, [app, False], pos=[10, 10], size=[50, 50])
+    button2 = Button(app, f, [app, True], pos=[70, 10], size=[50, 50])
+    menu1 = Menu(app, [button1, button2], pos=[10, 100], size=[130, 70])
+
     menus = [menu1]
     return menus
---
+++ texture.py
@@ -0,0 +1,6 @@
+from settings import *
+
+
+class Texture:
+    def __init__(self):
+        pass
