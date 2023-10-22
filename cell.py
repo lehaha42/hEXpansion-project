cd8f1d70651d0707affa59bfcbd65f1da2d40410
lehaha42 <shihovalexey2020@gmail.com>
2023-10-22 10:24:22 +1000

    update

--- cell.py
+++ cell.py
@@ -1,4 +1,5 @@
 from settings import *
+from button import Button
 
 
 SCALE = 0.6
@@ -12,11 +13,15 @@ teams = {
 
 
 class Cell:
-    def __init__(self, app, exist=True):
+    def __init__(self, app, pos, offset, scale, exist=True):
         self.app = app
         self.exist = exist
-        self.team = 'green'
+        self.team = 'none'
         self.connect = [False, False, False]
+        self.button = Button(self.app, self.atack, [],
+                             pos=[self.get_position(offset, pos, scale)[0] - scale*SCALE/2,
+                                  self.get_position(offset, pos, scale)[1] - scale*SCALE/2],
+                             size=[scale*SCALE, scale*SCALE])
 
     def update_connections(self, connects):
         self.connect = connects
@@ -27,10 +32,10 @@ class Cell:
         return [(i + j/2)*scale + offset[0], j*scale + offset[1]]
 
     def atack(self):
-        pass
+        self.team = 'green'
 
-    def update(self):
-        pass
+    def update(self, pos):
+        self.button.click(pos)
 
     def show(self, offset, pos, size):
         if self.exist:
@@ -44,3 +49,5 @@ class Cell:
             pg.draw.rect(self.app.screen, teams.get(self.team, GRAY), [cell_pos[0] - size * SCALE / 2 + 1,
                                                                        cell_pos[1] - size * SCALE / 2 + 1,
                                                                        size * SCALE, size * SCALE])
+            self.button.set_pos(new_pos=[self.get_position(offset, pos, size)[0] - size*SCALE/2,
+                                         self.get_position(offset, pos, size)[1] - size*SCALE/2])
