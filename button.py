76960cfb4fbc5ae87c7d3a78efb6106a15881919
lehaha42 <shihovalexey2020@gmail.com>
2023-10-22 10:22:45 +1000

    new func

--- button.py
+++ button.py
@@ -10,13 +10,16 @@ class Button:
         self.func_args = args
         self.exist = True
 
+    def set_pos(self, new_pos):
+        self.pos = new_pos
+
     def click(self, pos):
-        if self.pos[0] < pos[0] < self.pos[0] + self.size[0] and self.pos[1] < pos[1] < self.pos[1] + self.size[1] and self.exist:
+        if 0 < pos[0] - self.pos[0] < self.size[0] and 0 < pos[1] - self.pos[1] < self.size[1] and self.exist:
             self.func(*self.func_args)
 
     def show(self):
         if self.exist:
             pg.draw.rect(self.app.screen, WHITE, [*self.pos, *self.size], 3)
 
-    def set_exist(self, exist=True):
-        self.exist = exist
+    def set_exist(self, val=True):
+        self.exist = val
