6ac673961b2bc3c0700a4298e90baa13b9caea05
lehaha42 <shihovalexey2020@gmail.com>
2023-10-28 19:05:21 +1000

    smt

--- menus_builder.py
+++ menus_builder.py
@@ -5,18 +5,17 @@ from button import Button
 
 def build_menus(app):
 
-    def f(app, val):
-        app.is_running = val
 
     def f2(app):
         app.menus[0].set_exist(False)
         app.render_world = True
 
-    button1 = Button(app, f2, [app], pos=[810, 500], size=[300, 75])
-    button2 = Button(app, lambda : None, pos=[810, 625], size=[300, 75])
-    button3 = Button(app, lambda : None, pos=[810, 750], size=[300, 75])
-    button4 = Button(app, f, [app, False], pos=[810, 875], size=[300, 75])
-    title = Button(app, lambda : None, pos=[660, 300], size=[600, 175])
-    menu1 = Menu(app, [button1, button2, button3, button4, title], pos=[0, 0], size=WIN_RES)
-    menus = [menu1]
-    return menus
+    return [
+        Menu(app, [
+            Button(app, f2, [app], pos=[810, 500], size=[300, 75]),
+            Button(app, lambda: None, pos=[810, 625], size=[300, 75]),
+            Button(app, lambda: None, pos=[810, 750], size=[300, 75]),
+            Button(app, setattr, [app, 'is_running', False], pos=[810, 875], size=[300, 75]),
+            Button(app, lambda: None, pos=[660, 300], size=[600, 175])
+        ], pos=[0, 0], size=WIN_RES)
+    ]
